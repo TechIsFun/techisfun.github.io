21e948b4e17f936428c2495449dc1c2d6340307b
Andrea Maglie <pulsar316@gmail.com>
2014-08-18 17:03:49 +0200

    Update fragment-generator.js

--- pages/android-fragment-generator/fragment-generator.js
+++ pages/android-fragment-generator/fragment-generator.js
@@ -3,11 +3,37 @@ var app = angular.module('FragmentGenerator', [])
     $scope.$log = $log;
    
     $scope.argTypes = [
-      {name:'String', qualifiedName:'String', putMethod:'putString'},
-      {name:'Integer', qualifiedName:'Integer', putMethod:'putInteger'},
-      {name:'Long', qualifiedName:'Long', putMethod:'putLong'},
-      {name:'Double', qualifiedName:'Double', putMethod:'putDouble'},
-      {name:'Parcelable', qualifiedName:'android.os.Parcelable', putMethod:'putParcelable'}
+      {name:'Binder', qualifiedName:'android.os.IBinder'},
+      {name:'Boolean', qualifiedName:'boolean'},
+      {name:'BooleanArray', qualifiedName:'boolean[]'},
+      {name:'Bundle', qualifiedName:'android.os.Bundle'},
+      {name:'Byte', qualifiedName:'byte'},
+      {name:'ByteArray', qualifiedName:'byte[]'},
+      {name:'Char', qualifiedName:'char'},
+      {name:'CharArray', qualifiedName:'char[]'},
+      {name:'CharSequence', qualifiedName:'CharSequence'},
+      {name:'CharSequenceArray', qualifiedName:'CharSequence[]'},
+      {name:'CharSequenceArrayList', qualifiedName:'java.util.ArrayList<CharSequence>'},
+      {name:'Double', qualifiedName:'double'},
+      {name:'DoubleArray', qualifiedName:'double[]'},
+      {name:'Float', qualifiedName:'float'},
+      {name:'FloatArray', qualifiedName:'float[]'},
+      {name:'Int', qualifiedName:'int'},
+      {name:'IntArray', qualifiedName:'int[]'},
+      {name:'Integer', qualifiedName:'Integer'},
+      {name:'IntegerArrayList', qualifiedName:'java.util.ArrayList<Integer>'},
+      {name:'Long', qualifiedName:'long'},
+      {name:'LongArray', qualifiedName:'long[]'},
+      {name:'Parcelable', qualifiedName:'android.os.Parcelable'},
+      {name:'ParcelableArray', qualifiedName:'android.os.Parcelable[]'},
+      {name:'ParcelableArrayList', qualifiedName:'java.util.ArrayList<? extends android.os.Parcelable>'},
+      {name:'Serializable', qualifiedName:'Serializable'},
+      {name:'Short', qualifiedName:'short'},
+      {name:'ShortArray', qualifiedName:'short[]'},
+      {name:'SparseParcelableArray', qualifiedName:'android.util.SparseArray<? extends android.os.Parcelable>'},
+      {name:'String', qualifiedName:'String'},
+      {name:'StringArray', qualifiedName:'String[]'},
+      {name:'StringArrayList', qualifiedName:'java.util.ArrayList<String>'}
     ];
 
     $scope.fragmentArguments = [];
@@ -15,18 +41,39 @@ var app = angular.module('FragmentGenerator', [])
     // default values
     $scope.package = "";
     $scope.className = "";
+    $scope.argumentList = "";
 
     //$scope.generated_code_package = "";
 
     //$log.log($scope.package);
 
-     $scope.$watch('package', function (value) {
+    $scope.$watch('package', function (value) {
         $log.log($scope.package)
     });
 
-     $scope.$watch('className', function (value) {
+    $scope.$watch('className', function (value) {
         $log.log($scope.className)
     });
+
+    $scope.$watch('fragmentArguments', function (values) {
+        // create argument list
+        $scope.argumentList = "";
+        
+        angular.forEach(values, function(argument, key) {
+          $scope.argumentList += argument.argType.qualifiedName + " " + argument.name + ", ";
+        }, $log);
+
+        length = $scope.argumentList.length;
+        $scope.argumentList = $scope.argumentList.substring(0, length-2);
+        
+        //$log.log($scope.argumentList)
+        //$('#inputArgument{{$index}}').focus();
+    }, true)
+
+    $scope.addArgument = function($log) {
+      $scope.fragmentArguments.push({});
+    };
+
   }]);
 
 app.filter('capitalize', function() {
@@ -35,6 +82,20 @@ app.filter('capitalize', function() {
    return input.substring(0,1).toUpperCase()+input.substring(1);
  }
 });
+
+app.filter('stripExtension', function() {
+ return function(input, scope) {
+    if (input!=null) {
+      index = input.lastIndexOf(".");
+      if (index > -1) {
+        input = input.substring(0, index);
+      }
+    }
+   return input;
+ }
+});
+
+
 
 /*
 function SourceCodeGenerator($scope, $log) {
