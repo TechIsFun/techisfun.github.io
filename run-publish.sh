d138dda907ee976183a4f569b1c9383cb65d1afd
Andrea Maglie <andrea.maglie@gmail.com>
2020-08-01 19:42:26 +0200

    updated static site

---
+++ run-publish.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script per generare il sito e pubblicare il sito statico così generato su master
+
+echo "> Bundle install..."
+bundle install --path vendor/bundle
+
+echo "> Generate static site..."
+bundle exec jekyll build
+
+cd _site
+
+SITE_DIR=`pwd`
+
+cd ~/tmp/
+echo "> Clone repo..."
+git clone git@github.com:TechIsFun/techisfun.github.io.git
+cd techisfun.github.io
+echo "> Copy files..."
+cp -r $SITE_DIR/* .
+echo "> Commit..."
+git add -A
+git commit -a -m "updated static site"
+echo "> Push..."
+git push origin master
+cd ..
+echo "> Cleanup..."
+rm -rf techisfun.github.io
+echo "> Done"
